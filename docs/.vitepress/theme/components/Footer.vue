cee275754ab570bfd27266df71a0bedc8aad8d14
donghe <donghe587@139.com>
2025-06-10 19:25:27 +0800

    feat(docs): 为 Footer 组件添加深色主题支持
    - 在 Footer 组件中添加 NConfigProvider 以支持主题切换
    - 引入 darkTheme 并根据 isDark 值动态设置主题
    - 优化组件代码结构，提高可读性和可维护性

--- docs/.vitepress/theme/components/Footer.vue
+++ docs/.vitepress/theme/components/Footer.vue
@@ -1,18 +1,24 @@
 <template>
-  <h1 style="font-size: 24px;font-weight: bold;">
-    其他单位转换方法
-  </h1>
-  <n-card :bordered="false">
-    <n-grid x-gap="12" :cols="4">
-      <n-gi v-for="(file, index) in footer[lang]" :key="index">
-        <n-button text tag="a" :href="file.value" type="primary"> {{ file.label }} </n-button>
-      </n-gi>
-    </n-grid>
-  </n-card>
+  <h1 style="font-size: 24px;font-weight: bold;"> 其他单位转换方法 </h1>
+  <NConfigProvider  :theme="theme">
+    <n-card :bordered="false">
+      <n-grid x-gap="12" :cols="4">
+        <n-gi v-for="(file, index) in footer[lang]" :key="index">
+          <n-button text tag="a" :href="file.value" type="primary"> {{ file.label }} </n-button>
+        </n-gi>
+      </n-grid>
+    </n-card>
+  </NConfigProvider>
 </template>
 <script setup lang="ts">
-import { NButton, NForm, NFormItem, NInput, NInputNumber, NSelect, NCard, useMessage, NGrid, NGi } from 'naive-ui'
+import { NButton, NConfigProvider, darkTheme, NForm, NFormItem, NInput, NInputNumber, NSelect, NCard, useMessage, NGrid, NGi } from 'naive-ui'
 import { footer } from "./local";
-import { useData } from "vitepress";
-const { page, lang, site } = useData();
+import { useData, inBrowser } from "vitepress";
+import { watch, ref } from 'vue';
+const { page, lang, site, isDark } = useData();
+const theme = ref(null)
+watch(isDark, (dark) => {
+  if (!inBrowser) return;
+  theme.value = dark ? darkTheme : null;
+});
 </script>
