41b969b885656d0716c41dcbd68c53c092cd7c46
donghe <donghe587@139.com>
2025-06-11 14:27:32 +0800

    build(docs): 更新站点地图和 logo
    - 修改 sitemap 配置，将 hostname 更新为 https://unitconver.com/
    - 更新 docs/public/logo.svg 文件，替换为新的 logo 设计

--- docs/.vitepress/config/share.ts
+++ docs/.vitepress/config/share.ts
@@ -64,7 +64,7 @@ export const sharedConfig = defineConfig({
   },
   // 站点地图
   sitemap: {
-    hostname: 'https://vitepress-5tk.pages.dev',
+    hostname: 'https://unitconver.com/',
   },
   markdown: { // markdown 配置
     math: true,
